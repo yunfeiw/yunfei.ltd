1d3f49e1c29a4f1eb4b5cca381464ee106ff4312
yunfei.wang <yunfei.wang@easydatalink.com>
2020-09-22 20:15:07 +0800

    懒加载 动态标签

--- a.js
+++
@@ -1,7 +0,0 @@
-class a {
-    id;
-    Task(ID){id=ID}
-};
-class b inherits a {
-
-}
--- js/index.js
+++ js/index.js
@@ -5,27 +5,48 @@
  */
 
 function RenderList(obj) {
-    this.list = obj.arr;
+    this.root = obj.root;
+    this.list = obj.list;
     this.placeImg = obj.placeImg;
     // 初始化
+    this.init();
 }
 RenderList.prototype.init = function () {
     //骨架
-    this.frame();
+    this.frameUl();
 }
 RenderList.prototype.frameUl = function () {
     var _ul = document.createElement('ul');
     _ul.className = "goods_list clear_float";
+    var list = this.frameList();
+    _ul.appendChild(list);
+    this.root.appendChild(_ul);
 }
 RenderList.prototype.frameList = function () {
     var fragument = document.createDocumentFragment();
     var _list = this.list;
-    var placeImg = this.placeImg;
     for (var i = 0, len = _list.length; i < len; i++) {
-        var _li = document.createElement('li');
-        var _img = document.createElement('img');
-        _img.setAttribute("_src",_list['src']);
-        _img.setAttribute("src",placeImgf);
-        _li.appendChild(_img);
+        var _li = this.frameLi(_list[i]);
+        fragument.append(_li);
     };
+    return fragument;
+}
+RenderList.prototype.frameLi = function (item) {
+    var placeImg = this.placeImg;
+    var _li = document.createElement('li');
+    // create a link
+    var _a = document.createElement('a');
+    _a.setAttribute("href", item.href || "");
+    _a.setAttribute("title", item.title || "");
+    _a.setAttribute("target", "__brank");
+    // create img
+    var _img = document.createElement('img');
+    _img.setAttribute("_src", item['src']);
+    _img.setAttribute("src", placeImg);
+    _img.onload = function () {
+        _img.setAttribute("src", item['src']);
+    }
+    _a.appendChild(_img);
+    _li.appendChild(_a);
+    return _li;
 }
